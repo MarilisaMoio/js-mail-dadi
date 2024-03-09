c82d004f627b93ba2c446c3a267d2e749ed9e6c9
MarilisaMoio <moiomarilisa@gmail.com>
2024-03-09 18:32:00 +0100

    feat: add dice game

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,1 +1,17 @@
 //tirare un dado da 1 a 6 sia per l'utente che per il pc e decretare chi ha vinto
+
+//ottieni i due numeri
+const playerNumber = Math.floor(Math.random() * 6 + 1);
+
+const opponentNumber = Math.floor(Math.random() * 6 + 1);
+
+//definire il vincitore
+let message = "Pari!";
+
+if (playerNumber > opponentNumber){
+    message = "Hai vinto!"
+} else if (playerNumber < opponentNumber){
+    message = "Hai perso!"
+}
+
+console.log(playerNumber, opponentNumber, message)
